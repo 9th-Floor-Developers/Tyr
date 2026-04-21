ded77fcb31ee5a11aa3eed6eb98e79d252e6f260
Landen Ingerslev <lkingerslev@gmail.com>
2026-04-20 21:03:20 -0400

    Updated some logic and improved functionality
    - Project works on windows

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 import cv2
 import mediapipe as mp
 from cv2 import VideoCapture
@@ -8,11 +11,16 @@ from mediapipe.tasks.python.components.containers.landmark import \
 	NormalizedLandmark
 from mediapipe.tasks.python.vision.hand_landmarker import HandLandmarkerResult
 
+import pyautogui
+import math
+
 base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=1)
 detector = vision.HandLandmarker.create_from_options(options)
-BUFFER_DISTANCE: float = .05
-SCREEN_SIZE = (1920, 960)
+BUFFER_DISTANCE: float = .15
+SCREEN_SIZE = (1920, 1080)
+SMOOTHING = .8
+prev_x, prev_y = 0, 0
 
 
 def handle_mouse(hand: list[NormalizedLandmark]):
@@ -20,53 +28,77 @@ def handle_mouse(hand: list[NormalizedLandmark]):
 	index = hand[8]
 	middle = hand[12]
 	ring = hand[16]
-
-	if (thumb.x is None) or (index.x is None) or (middle.x is None) or (ring.x is None):
-		return
-
-	if (thumb.y is None) or (index.y is None) or (middle.y is None) or (ring.y is None):
-		return
-
-	print(f"{index.y} {thumb.y}")
 	
-	if distance(middle, thumb) < BUFFER_DISTANCE:
-		print("left click")
-	elif distance(ring, thumb) < BUFFER_DISTANCE:
-		print("right click")
-	if index.y < thumb.y:
-		print("hold")
-		#pyautogui.moveTo(index.x * SCREEN_SIZE[0], index.y * SCREEN_SIZE[1])
+	if not any(
+			[
+				thumb.x, thumb.y,
+				index.x, index.y,
+				middle.x, middle.y,
+				ring.x, ring.y
+			]
+	):
+		return
+	
+	global prev_x, prev_y
+	
+	# print(f"{index.y} {thumb.y}")
+	
+	if distance(middle, thumb) < BUFFER_DISTANCE:  # left click
+		pass
+		# pyautogui.mouseDown(button='left')
+	elif distance(ring, thumb) < BUFFER_DISTANCE:  # right click
+		pass
+		# pyautogui.mouseDown(button='right')
+	
+	if index.y < thumb.y:  # movement
+		target_x = (1 - index.x) * SCREEN_SIZE[0]
+		target_y = index.y * SCREEN_SIZE[1]
+		
+		smooth_x = prev_x + (target_x - prev_x) * SMOOTHING
+		smooth_y = prev_y + (target_y - prev_y) * SMOOTHING
+		
+		pyautogui.moveTo(smooth_x, smooth_y)
+		
+		prev_x, prev_y = smooth_x, smooth_y
 
 
 def distance(
 	landmark1: NormalizedLandmark,
 	landmark2: NormalizedLandmark
 ) -> float:
-	if (landmark1.x is None) or (landmark2.x is None):
-		return 10000 #If 1 of the 2 numbers are null
-
-	if (landmark1.y is None) or (landmark2.y is None):
-		return 10000
-
+	if not any([landmark1.x, landmark1.y, landmark2.x, landmark2.y]):
+		return math.inf
+	
 	return abs(landmark2.y - landmark1.y) + abs(landmark2.x - landmark1.x)
 
 
-def main():
+def detect_hand(mp_image: Image) -> None:
+	detection_result: HandLandmarkerResult = detector.detect(mp_image)
+	
+	if len(detection_result.hand_landmarks) != 0:
+		hand: list[NormalizedLandmark] = detection_result.hand_landmarks[0]
+		handle_mouse(hand)
+
+
+def main() -> None:
 	cap: VideoCapture = cv2.VideoCapture(0)
 	
 	while True:
-		ret, frame = cap.read()
+		_, frame = cap.read()
 		
 		frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 		mp_image: Image = mp.Image(
 			image_format=mp.ImageFormat.SRGB, data=frame_rgb
 		)
 		
-		detection_result: HandLandmarkerResult = detector.detect(mp_image)
+		thread = threading.Thread(target=detect_hand, args=(mp_image,))
+		# start_time = time.perf_counter()
+		thread.start()
+		# thread.join()
+		# end_time = time.perf_counter()
 		
-		if len(detection_result.hand_landmarks) != 0:
-			hand: list[NormalizedLandmark] = detection_result.hand_landmarks[0]
-			handle_mouse(hand)
+		# print(end_time - start_time)
+		# time.sleep(.15)
 		
 		cv2.imshow("Camera Feed", frame)
 		
